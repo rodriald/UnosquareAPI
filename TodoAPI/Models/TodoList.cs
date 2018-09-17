2635a0d205aee5e8af6e44d1e1337163f9581726
Aldo Rodriguez <aldo.rodriguez@unosquare.com>
2018-09-17 17:36:15 -0500

    Add TodoList entity

---
+++ TodoAPI/Models/TodoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoList
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Comments { get; set; }
+        public long UserId { get; set; }
+        public List<TodoItem> ItemsList { get; set; }
+    }
+}
